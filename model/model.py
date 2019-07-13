1767a2ef81e6bd2b697bef10e80bbd79c3f07043
AppleCrazy <15635979+applecrazy@users.noreply.github.com>
2019-07-13 17:43:56 -0500

    Implement encoder

--- model/model.py
+++ model/model.py
@@ -7,11 +7,15 @@ class NewsSummarizationModel:
     summaries = None
     summaries_words = None
     summaries_words_reverse = None
+    summary_vocab_size = None
     documents = None
     documents_words = None
     documents_words_reverse = None
+    document_vocab_size = None
     encoder = None
     decoder = None
+    fixed_vector_dim = 200
+    embedding_dim = 128
 
     def __init__(self, pickled_data_path: str = '../data/'):
         # TODO: offload data loading to different functions to be called within the __init__ function
@@ -20,27 +24,28 @@ class NewsSummarizationModel:
         with open(pickle_path, 'rb') as fi:
             (self.documents, self.documents_words, self.documents_words_reverse),\
             (self.summaries, self.summaries_words, self.summaries_words_reverse) = pickle.load(fi)
-            self.encoder['n'] = len(self.documents[0])
-            self.encoder['m'] = len(self.documents)
-            self.decoder['n'] = len(self.decoder[0])
-            self.decoder['m'] = len(self.decoder)
         self.encoder = {
             'n': len(self.documents[0]),
             'm': len(self.documents),
-            'layers': {}
+            'input': None,
+            'lstm': None,
+            'states': None
         }
         self.decoder = {
             'n': len(self.summaries[0]),
             'm': len(self.summaries),
-            'layers': {}
+            'input': None,
+            'lstm': None,
+            'states': None
         }
+        self.document_vocab_size = len(self.documents_words)
+        self.summary_vocab_size = len(self.summaries_words)
 
     def build_encoder(self):
-        # TODO: implement encoder layers as per spec
-        self.encoder.layers = {
-            'input': keras.layers.Input(shape=(None, self.encoder.n)),
-            # 'embedding': keras.layers.Embedding()
-        }
+        self.encoder['input'] = keras.layers.Input(shape=(None, self.encoder['n']))
+        self.encoder['lstm'] = keras.layers.LSTM(self.fixed_vector_dim, return_sequences=True, return_state=True)
+        enc_out, h, c = self.encoder['lstm'](self.encoder['input'])
+        self.encoder['states'] = [h, c]
 
     def build_decoder(self):
         # TODO: implement decoder
@@ -57,3 +62,7 @@ class NewsSummarizationModel:
     def view_summary_text(self):
         # TODO: translate a sequence of numerical summary tokens to actual text
         pass
+
+if __name__ == '__main__':
+    model = NewsSummarizationModel()
+    model.build_encoder()
