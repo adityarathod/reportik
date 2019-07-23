e0b4cf8af0e35be0d4d2dde5d6b043a2048ad4e9
AppleCrazy <15635979+applecrazy@users.noreply.github.com>
2019-07-23 11:58:43 -0500

    Clean up scraper

--- scraper/cnbc_scraper.py
+++ scraper/cnbc_scraper.py
@@ -1,31 +1,29 @@
-import requests
-import time
-import newspaper
-import hashlib
 from bs4 import BeautifulSoup
 from multiprocessing.dummy import Pool as ThreadPool
+import requests
+import time
+import os
+import newspaper
+import re
+import hashlib
 
 
 class CNBCScraper:
     links = None
     logging = None
-    sitemap_url = 'https://www.cnbc.com/CNBCsitemapAll9.xml'
+    documents_path = None
+    summaries_path = None
 
-    def __init__(self, sitemap_path: str = None, logging: bool = True):
+    def __init__(self, sitemap_path, logging: bool = True):
         self.logging = logging
-        if sitemap_path is not None:
-            with open(sitemap_path) as f:
-                sitemap = f.read()
-                soup = BeautifulSoup(sitemap, 'xml')
-                self.links = [x.string for x in soup.find_all('loc')]
-        else:
-            sitemap = requests.get(self.sitemap_url)
-            soup = BeautifulSoup(sitemap.text(), 'xml')
+        with open(sitemap_path) as f:
+            sitemap = f.read()
+            soup = BeautifulSoup(sitemap, 'xml')
             self.links = [x.string for x in soup.find_all('loc')]
 
     @staticmethod
     def generate_doc_id(title: str):
-        md5 = hashlib.md5(f'{title}{str(int(time.time()))}'.encode())
+        md5 = hashlib.md5(f'{title}'.encode())
         return md5.hexdigest()
 
     def log(self, msg, end='\n'):
@@ -42,27 +40,30 @@ class CNBCScraper:
         tmp = requests.get(link)
         soup = BeautifulSoup(tmp.text, 'html.parser')
         points = soup.find('div', class_='KeyPoints-list')
-        if points != None:
+        if points is not None:
             article = newspaper.Article(link)
             article.download()
             article.parse()
             key_pts = points.text
-            article_id = CNBCScraper.generate_doc_id(article.title)
-            with open(f'../data/texts/{article_id}.txt', 'w+') as f:
+            key_pts = re.sub(r'\.(?=[^ \W\d])', '. ', key_pts)
+            article_id = self.generate_doc_id(article.title)
+            with open(os.path.join(self.documents_path, str(article_id) + '.txt'), 'w+') as f:
                 f.write(article.text)
-            with open(f'../data/points/{article_id}.txt', 'w+') as f:
+            with open(os.path.join(self.summaries_path, str(article_id) + '.txt'), 'w+') as f:
                 f.write(key_pts)
-            self.log(f'Downloading article #{idx + 1}...', end='')
-            self.log(f'"{article.title}"...', end='')
-            self.log('done.')
+            self.log(f'Downloading article #{idx + 1}: "{article.title}..."')
         else:
             self.log(f'Skipping article #{idx + 1}...{link}')
 
-    def scrape(self):
+    def scrape(self, documents_path, summaries_path):
+        self.documents_path = documents_path
+        self.summaries_path = summaries_path
         pool = ThreadPool(4)
         _ = pool.map(self.process_link, enumerate(self.links))
 
 
 if __name__ == '__main__':
-    scraper = CNBCScraper(sitemap_path='cnbc_sitemap_r1.xml')
-    scraper.scrape()
+    scraper = CNBCScraper('cnbc_sitemap_2018.xml')
+    doc_path = os.path.join('..', 'data', 'documents')
+    sum_path = os.path.join('..', 'data', 'summaries')
+    scraper.scrape(documents_path=doc_path, summaries_path=sum_path)
