fdb735991ab81e6527755a02b82e2a5346114bf2
AppleCrazy <15635979+applecrazy@users.noreply.github.com>
2019-07-27 17:22:24 -0500

    Clean up Pipfile, fix more bugs

---
+++ install_supplemental_pkgs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Installing seq2seq and recurrentshop..."
+!pip3 install git+https://www.github.com/farizrahman4u/recurrentshop.git
+!pip3 install git+https://github.com/farizrahman4u/seq2seq.git
+echo "Done."
