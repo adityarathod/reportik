3ff577534c296cecbc31e5e32845e46ba3f80cf5
AppleCrazy <15635979+applecrazy@users.noreply.github.com>
2019-07-27 11:20:04 -0500

    Learn embeddings using FastText

--- doc_processor/embeddings.py
+++ doc_processor/embeddings.py
@@ -1,27 +1,35 @@
-import pickle
+from doc_processor.processor import DocumentCleaner
 import fasttext
-import numpy as np
-
-embeddings_file = '../data/wiki-news-300d-1M.vec'
-
-def load_data(file='../data/cnbc_data.pkl'):
-    with open(file, 'rb') as fi:
-        return pickle.load(fi)
-
-def lookup_document_word(word_idx):
-    return documents_words_reverse[word_idx]
+import os
 
 
-(documents, documents_words, documents_words_reverse), \
-(summaries, summaries_words, summaries_words_reverse) = load_data()
+def learn_embeddings(texts_dir='../data/documents', summaries_dir='../data/summaries'):
+    print('Processing files located in', texts_dir, '...')
+    document_file = open('./tmp-doc-emb.txt', 'w+')
+    for file in os.listdir(texts_dir):
+        if DocumentCleaner.is_hidden_file(file):
+            continue
+        with open(os.path.join(texts_dir, file), 'r', errors='ignore') as f:
+            document_file.write(f.read() + '\n\n\n')
+    print('Done.')
+    document_file.close()
+    print('Training word embeddings...')
+    model = fasttext.train_unsupervised('./tmp-doc-emb.txt')
+    model.save_model('doc_emb.bin')
+    print('Done.')
+    print('Processing files located in', summaries_dir, '...')
+    summ_file = open('./tmp-summ-emb.txt', 'w+')
+    for file in os.listdir(summaries_dir):
+        if DocumentCleaner.is_hidden_file(file):
+            continue
+        with open(os.path.join(summaries_dir, file), 'r', errors='ignore') as f:
+            summ_file.write(f.read() + '\n\n\n')
+    print('Done.')
+    summ_file.close()
+    print('Training word embeddings...')
+    model = fasttext.train_unsupervised('./tmp-summ-emb.txt')
+    model.save_model('summ_emb.bin')
+    print('Done.')
 
-document_model = fasttext.load_model('../data/document_embeddings.bin')
-
-docs_trans = np.zeros(shape=(len(documents), len(documents[0]), 100))
-
-for idx, doc in enumerate(documents):
-    print(f'processing document {idx}...')
-    v = np.array([document_model.get_word_vector(lookup_document_word(x)) for x in doc])
-    docs_trans[idx] = v
-
-print(docs_trans.shape)
+if __name__ == '__main__':
+    learn_embeddings()
